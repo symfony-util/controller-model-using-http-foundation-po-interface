f556cad421095dae847b6931e01ab435a6261c57
Nardberjean <Nardberjean@users.noreply.github.com>
2017-09-04 11:45:01 -0400

    Update NullActionModel.php

--- NullActionModel.php
+++ NullActionModel.php
@@ -12,27 +12,11 @@
 namespace SymfonyUtil\Component\HttpFoundationPOInterface;
 
 use Symfony\Component\HttpFoundation\Request;
-use SymfonyUtil\Component\HttpFoundation\ControllerModelInterface;
 
-class ControllerModel implements ControllerModelInterface // interface in SU//HttpF
+class NullActionModel
 {
-    protected $actionModel; // interface in (SU//HttpF) + POI(return type (php 7 -> s4 + php 7.1)
-    protected $viewModel; // interface in POI
-
-    public function __construct($actionModel, $viewModel)
-    {
-        $this->actionModel = $actionModel;
-        $this->viewModel = $viewModel;
-    }
-
     public function __invoke(Request $request = null)
     {
-        return new ResponseParameters($this->viewModel($this->actionModel($request)));
-
-        // $actionResult = $this->actionModel($request); // resturns ResponseMixedInterface
-
-        // return new ResponseParameters($this->viewModel($actionResult->getViewModelParameters()), $actionResult->getResponse());
-
-        // return $this->actionModel($request)->filter($this->viewModel);
+        return [];
     }
 }
