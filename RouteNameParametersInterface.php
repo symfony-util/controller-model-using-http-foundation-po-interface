72910b31874808d34feaa6070c86af1b8a12bf6e
Nardberjean <Nardberjean@users.noreply.github.com>
2017-09-06 17:27:50 -0400

    Create RouteNameParametersInterface.php

---
+++ RouteNameParametersInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Symfony-Util package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SymfonyUtil\Component\HttpFoundationPOInterface;
+// TODO: Move to POI
+
+interface RouteNameParametersInterface
+{
+    public function getRoute(); // string
+
+    public function getRouteParameters(); // array
+
+    public function getViewModelParameters(); // array
+}
